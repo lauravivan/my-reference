74aa22804986570484bfc3d84ca0a1031f1a7d2e
lauravivan <lauvivang@gmail.com>
2024-05-31 20:57:57 -0300

    Add store to store references

---
+++ src/store/store.ts
@@ -0,0 +1,28 @@
+const store = [
+  {
+    category: "Books",
+    subCategories: [
+      {
+        subCategory: "Programming Language",
+        references: [
+          {
+            name: "Typescript in 50 lessons by Stefan Baumgartner",
+            link: "https://typescript-book.com/",
+            img: "typescript-in-50-lessons.jpg",
+          },
+        ],
+      },
+      {
+        subCategory: "Design",
+        references: [
+          {
+            name: "Don't make me think by Steve Krug",
+            img: "",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default store;
