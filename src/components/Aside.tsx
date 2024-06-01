fcac4802fbe75463569c2fff05d5e1e2e20cd830
lauravivan <lauvivang@gmail.com>
2024-05-31 21:02:29 -0300

    Add Hello and Menu components

---
+++ src/components/Aside.tsx
@@ -0,0 +1,13 @@
+import { Hello, Menu } from "./index";
+
+export function Aside() {
+  return (
+    <aside>
+      <Hello>
+        Hi, I’m Laura. Welcome to my library of references in Web Development
+        and Tech World 👋
+      </Hello>
+      <Menu />
+    </aside>
+  );
+}
