292ee2a5871083f45cf3ae186ceb91f589c24ed3
lauravivan <lauvivang@gmail.com>
2024-05-31 21:09:27 -0300

    Update page main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import "./App.css";
-import { IKImage } from "imagekitio-react";
-import { Hello } from "./components";
+import { Aside, Main } from "./components";
 
 function App() {
   return (
-    // <IKImage
-    //   urlEndpoint={import.meta.env.VITE_URL_ENDPOINT}
-    //   path="typescript-in-50-lessons.jpg"
-    // />
-    <div>
-      <Hello>
-        Hi, I’m Laura. Welcome to my library of references in Web Development
-        and Tech World 👋
-      </Hello>
-    </div>
+    <>
+      <Aside />
+      <Main />
+    </>
   );
 }
 
